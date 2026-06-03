a4e3e9116824e652efa82d31fc3f675547efbe67
Balaraman-007-av <balaraman.av2025cse@sece.ac.in>
2026-06-03 21:13:21 +0530

    adding

---
+++ Bio.java
@@ -0,0 +1,8 @@
+class Bio{
+    public static void main(String[] args) {
+        System.out.println("Name: Balaraman A V");
+        System.out.println("Age: 17");
+        System.out.println("Department: CSE");
+        System.out.println("CGPA: 8.14");
+    }
+}	
